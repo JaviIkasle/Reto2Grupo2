3cef1da2785aeaf11c696958fd4fb492e620f7c8
in2dm3-v <in2dm3-v@INFHERBE14.elorrieta.int>
2022-12-19 16:50:46 +0100

    Commit inicial

---
+++ Reto2-Grupo2/src/main/java/com/example/Reto2Grupo2/Reto2Grupo2Application.java
@@ -0,0 +1,13 @@
+package com.example.Reto2Grupo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Reto2Grupo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Reto2Grupo2Application.class, args);
+	}
+
+}
---
+++ Reto2-Grupo2/src/test/java/com/example/Reto2Grupo2/Reto2Grupo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Reto2Grupo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Reto2Grupo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
