7f1fa3c5b53b6e68bf3b8cdace214ef270bee851
in2dm3-v <in2dm3-v@INFHERBE14.elorrieta.int>
2022-12-19 21:05:42 +0100

    Relaciones de clases 1:N hechas

--- Reto2-Grupo2/src/main/java/com/example/Reto2Grupo2/animal/modelo/Animal.java
+++ Reto2-Grupo2/src/main/java/com/example/Reto2Grupo2/animal/modelo/Animal.java
@@ -1,10 +1,18 @@
 package com.example.Reto2Grupo2.animal.modelo;
 
+import com.example.Reto2Grupo2.especie.modelo.Especie;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -19,6 +27,15 @@ public class Animal {
 	@Column(length = 250)
 	private String informacion;
 	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "id_especie", foreignKey=@ForeignKey(name = "FK_id_especie"))
+	@JsonManagedReference
+	@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+	private Especie especie;
+	
+	@Column(name="id_especie", insertable=false, updatable=false)
+	private Integer idEspecie;
+	
 	public Animal() {}
 
 	public Animal(Integer id, String nombre, String informacion) {
@@ -26,6 +43,15 @@ public class Animal {
 		this.id = id;
 		this.nombre = nombre;
 		this.informacion = informacion;
+	}
+	
+	public Animal(Integer id, String nombre, String informacion, Especie especie, Integer idEspecie) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.informacion = informacion;
+		this.especie = especie;
+		this.idEspecie = idEspecie;
 	}
 
 	public Integer getId() {
@@ -52,8 +78,25 @@ public class Animal {
 		this.informacion = informacion;
 	}
 
+	public Especie getEspecie() {
+		return especie;
+	}
+
+	public void setEspecie(Especie especie) {
+		this.especie = especie;
+	}
+
+	public Integer getIdEspecie() {
+		return idEspecie;
+	}
+
+	public void setIdEspecie(Integer idEspecie) {
+		this.idEspecie = idEspecie;
+	}
+
 	@Override
 	public String toString() {
-		return "Animal [id=" + id + ", nombre=" + nombre + ", informacion=" + informacion + "]";
+		return "Animal [id=" + id + ", nombre=" + nombre + ", informacion=" + informacion + ", especie=" + especie
+				+ ", idEspecie=" + idEspecie + "]";
 	}
 }
