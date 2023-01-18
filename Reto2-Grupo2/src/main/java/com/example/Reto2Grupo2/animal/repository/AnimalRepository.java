330290bda2cedd7c2d26758dbb01e5ee94466926
in2dm3-v <in2dm3-v@INFHERBE14.elorrieta.int>
2023-01-18 17:44:28 +0100

    Se quitan comentarios

--- Reto2-Grupo2/src/main/java/com/example/Reto2Grupo2/animal/repository/AnimalRepository.java
+++ Reto2-Grupo2/src/main/java/com/example/Reto2Grupo2/animal/repository/AnimalRepository.java
@@ -10,9 +10,6 @@ import com.example.Reto2Grupo2.animal.modelo.Animal;
 
 public interface AnimalRepository extends CrudRepository<Animal, Integer>, JpaRepository<Animal, Integer>{
 
-	//Iterable<Animal> getAnimalesFromZoo(Integer id);
-
-//	@Query("select animal_id from zoo_has_animal where zoo_id =:idZoo")
 	Iterable<Animal> findAnimalByZoosId(@Param("idZoo") Integer id);
 	
 }
